7e3ff3c020eafe29696e1d652d56c6a22a3bf5c1
Natasha Ferguson <59899329+nata-ferguson@users.noreply.github.com>
2020-08-03 13:32:59 -0500

    Add files via upload

---
+++ ui-diary-page/index.js
@@ -0,0 +1,36 @@
+const entryForm = document.getElementById('entryForm');
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
+
+let count = 1;
+
+function addEntryToDom(event){
+    event.preventDefault();
+    const entryDiv = document.createElement('div');
+    entryDiv.className = "single-entry";
+    entryDiv.innerText = entryTextbox.value;
+    entryDiv.style.display = 'none';
+    entriesSection.appendChild(entryDiv);
+    entryTextbox.value = "";
+
+    const displayEntryButton = document.createElement('button');
+    displayEntryButton.className = 'display-entry-button';
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton);
+
+    displayEntryButton.addEventListener('click', function() {
+        let allEntries = document.querySelectorAll('.single-entry');
+        for (let i=0; i < allEntries.length; i++) {
+            allEntries[i].style.display = 'none'; 
+        }
+        entryDiv.style.display = 'block';
+    })
+
+    count++
+
+}
+
+entryForm.addEventListener("submit", addEntryToDom)
+
+
